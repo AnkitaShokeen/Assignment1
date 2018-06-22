17c4552e5b38f5c6836e73714983509efbae859d
AnkitaShokeen <40365679+AnkitaShokeen@users.noreply.github.com>
2018-06-22 12:19:22 +0530

    Add files via upload

---
+++ assignment02.py
@@ -0,0 +1,59 @@
+
+QUESTION:1  Print Something
+
+x="My name is Ankita Shokeen"
+y="My age is 21"
+print(x,"\n",y)
+
+
+QUESTION:2  Join Strings
+
+#concatenation
+x="face"
+y="book"
+print(x+y)
+a="My name is Ankita."
+b=" I am 21 years old."
+print(a+b)
+
+
+QUESTION:3 Take the input of 3 variables x, y and z . Print their values on screen.
+
+x=input("Enter your name : ")
+y=int(input("Enter your age : "))
+z=input("Enter your city : ")
+print(x)
+print(y)
+print(z)
+
+
+QUESTION:4  Let's get started on screen.
+
+x="Let's get started"
+print(x)
+
+
+QUESTION:5 Print the following values using placeholders.
+s=”Acadview”
+course=”Python”
+fees=5000
+
+s = "Acadview"
+course="Python"
+fees= 5000
+print("The source is : %s" %(s))
+print("The course is : %s" %(course))
+print("The fees is :  %d" %(fees))
+
+
+QUESTION:6 Let’s do some interesting exercise:
+name=”Tony Stark”
+salary=1000000
+
+print(‘%s’’%d’)%(____ ,____)
+name = "Tony Stark"
+salary=1000000
+print("Name is %s and Salary is %d" %(name,salary))
+
+
+
