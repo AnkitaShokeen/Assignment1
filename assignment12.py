1c54857c56e843f9df24595fd0b942ead6c6202f
Ankita Shokeen <40365679+AnkitaShokeen@users.noreply.github.com>
2018-06-29 00:50:55 +0530

    Add files via upload
    API

---
+++ assignment12.py
@@ -0,0 +1,46 @@
+#Q.1- Difference between JSON and XML files?
+
+1)JSON stands for JavaScript Object Notation and XML stands for eXtensible Markup Language.
+2)XML is a markup language (as it actually says in its name), whereas JSON is a way of representing objects (as also noted in its name).
+3)JSON is simple to read and write whereas XML is less simple than JSON.
+4)JSON is data-oriented whereas XML is document-oriented.
+5)JSON doesn't provide display capabilities whereas XML provides the capability to display data because it is a markup language.
+6)JSON supports array whereas XML doesn't support array.
+7)JSON is less secured than XML whereas XML is more secured.
+8)JSON supports only text and number data type whereas XML support many data types such as text, number, images, charts, graphs etc.
+Moreover, XML offeres options for transferring the format or structure of the data with actual data.
+
+
+#Q.2- Get the IP address of some common sites like Google, Facebook by using DNS lookup.
+
+Google- 172.217.161.14
+Facebook- 157.240.16.35
+
+
+#Q.3-How http works?                
+
+HTTP is a request response protocol to communicate asynchrnously between client and server.
+Mostly in HTTP a browser acts as a client and a web-server like Apache or IIS acts as server.
+The server which hosts the files (like html , audio , video files etc) responses to the client.
+Depending on the request a response contains the status of the request.
+HTTP has formalized on a few that capture the essentials that are universally applicable for all kinds of applications.
+
+These request verbs are:
+
+GET: fetch an existing resource. The URL contains all the necessary information the server needs to locate and return the resource.
+POST: create a new resource. POST requests usually carry a payload that specifies the data for the new resource.
+PUT: update an existing resource. The payload may contain the updated data for the resource.
+DELETE: delete an existing resource.
+The above four verbs are the most popular, and most tools and frameworks explicitly expose these request verbs. PUT and DELETE are sometimes considered specialized versions of the POST verb, and they may be packaged as POST requests with the payload containing the exact action: create, update or delete.
+
+
+#Q.4- What is a difference between library and API . Figure it out with examples.
+
+Library: A library is a collection of functions / objects that serves one particular purpose. you could use a library in a variety of projects. (Various specialized services in our case)
+API: An API(Application Programming Language) is an interface for other programs to interact with your program or library  without having direct access. ( giving specifications for our need to various vendors in our case)
+API: functionality of an API is exposed to be further used.
+Library: functionalities aren't exposed.
+
+
+#Q.5- Try to access Spotify API . Find out some library for it and play some music.
+Sir told not to do this question.
